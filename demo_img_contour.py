5698761f657173495c8b7032ff90666f1b606b02
Lyndon Li <Lyndon.Leee@outlook.com>
2025-08-25 02:04:12 +0800

    *: split the draw function into a stand alone one

--- demo_img_contour.py
+++ demo_img_contour.py
@@ -1,5 +1,16 @@
 import cv2
 
+def draw_contour(img,contours):
+    for contour in contours:
+        print(contour)
+        print(f'Area: {cv2.contourArea(contour)}, Length: {cv2.arcLength(contour, True)}')
+        print('----------------------')
+        # cv2.putText(img, f"Area: {cv2.contourArea(contour)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
+        for point in contour:
+            print(point)
+            x,y = point[0]
+            cv2.circle(img, (x, y), 5, (0,255,0), 1)
+
 img = cv2.imread("assets/contour.png")
 cv2.imshow("img",img)
 
@@ -12,16 +23,7 @@ cv2.imshow("binary",binary)
 contours,hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # print(contours)
 cv2.drawContours(img, contours, -1, (0,0,255), 1)
-
-for contour in contours:
-    print(contour)
-    print(f'Area: {cv2.contourArea(contour)}, Length: {cv2.arcLength(contour, True)}')
-    print('----------------------')
-    # cv2.putText(img, f"Area: {cv2.contourArea(contour)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
-    for point in contour:
-        print(point)
-        cv2.circle(img, (point[0][0], point[0][1]), 5, (0,255,0), 3)
-
+draw_contour(img, contours)
 cv2.imshow("contours",img)
 
 key = cv2.waitKey(0)
